5422ba72143c30c289a81614b8f2a13ad1fc6e1f
Robert <rlhambley@mail.fhsu.edu>
2020-05-06 09:44:02 -0500

    security

--- view/administrators.php
+++ view/administrators.php
@@ -16,7 +16,7 @@ $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
     $action = filter_input(INPUT_GET, 'action');
     if ($action == NULL) {
-        $action = 'administrators';
+        $action = 'register';
     }
 }
 //echo $action;
